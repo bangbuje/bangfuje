1ec409eedf23d43b3a14a0470db7392649e543d4
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2024-12-11 19:44:09 +0900

    Chore: Headwind 적용

--- src/components/Landing/DownArrow.tsx
+++ src/components/Landing/DownArrow.tsx
@@ -5,7 +5,7 @@ export default function DownArrow() {
 	return (
 		<Link href="/auth/login">
 			<div className="cursor-pointer">
-				<img src="/downArrow.svg" alt="Down Arrow" className="absolute inset-x-0 bottom-8 mx-auto w-16 h-auto animate-float" />
+				<img src="/downArrow.svg" alt="Down Arrow" className="absolute inset-x-0 w-16 h-auto mx-auto bottom-8 animate-float" />
 			</div>
 		</Link>
 	)
